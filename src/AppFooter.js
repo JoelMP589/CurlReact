af5800dc1bc66d846d979f7fc72eaf5da880c4fd
Jonathan <jonathan@mp.com>
2021-10-05 14:59:37 -0500

    cambio de logo

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -5,7 +5,7 @@ const AppFooter = () => {
     return (
         <div className="layout-footer">
             <div className="footer-logo-container">
-                <img id="footer-logo" src="assets/layout/images/birbLogoFondoTransparente.png" alt="diamond-layout" />
+                <img id="footer-logo" src="assets/layout/images/LogoTransparente.png" alt="diamond-layout" />
                 <span className="app-name">CURL</span>
             </div>
             <span className="copyright">&#169; JNS - 2021</span>
